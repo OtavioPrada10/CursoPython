df56830ca6bdde88cfe707e37051d10f714339b7
OtavioPrada10 <otaviovinicius07@gmail.com>
2023-09-01 15:09:20 -0300

    entrada

---
+++ aula_38.py
@@ -0,0 +1,7 @@
+iValor1 = input('Digite o Valor 1: ')
+iValor2 = input('Digite o Valor 2: ')
+
+if (iValor1 > iValor2):
+    print('Valor 1 é o maior: ', iValor1)
+else:
+    print('Valor 2 é o maior: ', iValor2)
