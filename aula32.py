af46b7a108d66e9029433b1e263d28bf92cbbacb
Otavio Prada <142634808+OtavioPrada10@users.noreply.github.com>
2023-09-21 08:05:21 -0300

    Add files via upload

---
+++ aula32.py
@@ -0,0 +1,14 @@
+# iNumero = input('Digite um numero inteiro: ')
+
+# try:
+#     iNumero = int(iNumero)
+# except:
+#     print('O numero não é do tipo inteiro')
+#     exit()
+
+# isPar = iNumero % 2 == 0
+
+# if(isPar):
+#     print('O numero é par')
+# else:
+#     print('O numero é impar')
---
+++ aula32_atividade2.py
@@ -0,0 +1,12 @@
+def getHora():
+    print('')
+
+sHoraInformada = input('Digite o Horario (formato: 10-05): ')
+
+getHora(sHoraInformada)
+
+if():
+    print()
+
+
+
